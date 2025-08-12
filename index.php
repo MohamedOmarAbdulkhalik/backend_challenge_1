a5ce4d335b2606e0dbc30cf244bd4b9a963d857d
Mohammed Omar Abdulkhaleq Esmail <mohamedalaghbari20@gmail.com>
2025-08-12 22:44:21 +0300

    feat(index.php): show server date and time

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php
-echo "Hello";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1> <?php echo "Today Date: " . date("Y-m-d");
+            echo "<br>";
+            echo "Time: " . date("H:i:s"); ?> </h1>
+</body>
+
+</html>
